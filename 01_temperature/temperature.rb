e92089d19c2773832c91927bb0d196be24b8e2fa
Zanemonkey <z.mather@gmail.com>
2017-01-20 22:24:34 +1300

    Implement hello, temperature and calculator

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,14 @@
 #write your code here
+
+def ftoc (temperature)
+
+	(temperature - 32) * 5 / 9	
+
+end
+
+def ctof (temperature) 
+
+	#temperature = temperature.to_f
+	(temperature / (5.0 / 9.0)) + 32
+
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+
+def add (x, y)
+	x + y
+end
+
+def subtract (x, y)
+	x - y
+end
+
+def sum (array)
+	result = 0
+	array.each do |i|
+		result += i
+	end
+	result
+end
+
+def multiply (array)
+	result = 1
+	array.each do |i|
+		result *= i
+	end
+	result
+end
+
+def power (x, y)
+	x**y
+end
+
+def factorial (x)
+	result = 1
+	i = 2
+	while (i <= x)
+		result *= i
+		i += 1
+	end
+	result
+end
